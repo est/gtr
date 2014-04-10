8f176a816a8558f296198381885f210758bf5359
est <electronicstar@126.com>
2014-04-10 16:37:32 +0800

    tcp ping demo

--- src/gtr.go
+++ src/gtr.go
@@ -8,13 +8,16 @@
 package main
 
 import (
-    "bytes"
-    "flag"
+    // "bytes"
+    // "flag"
     "fmt"
-    "log"
+    // "log"
     "net"
-    "os"
+    // "os"
     "time"
+    "syscall"
+    "reflect"
+    "errors"
 )
 
 const (
@@ -22,106 +25,44 @@ const (
     ICMP_ECHO_REPLY   = 0
 )
 
-// returns a suitable 'ping request' packet, with id & seq and a
-// payload length of pktlen
-func makePingRequest(id, seq, pktlen int, filler []byte) []byte {
-    p := make([]byte, pktlen)
-    copy(p[8:], bytes.Repeat(filler, (pktlen-8)/len(filler)+1))
 
-    p[0] = ICMP_ECHO_REQUEST // type
-    p[1] = 0                 // code
-    p[2] = 0                 // cksum
-    p[3] = 0                 // cksum
-    p[4] = uint8(id >> 8)    // id
-    p[5] = uint8(id & 0xff)  // id
-    p[6] = uint8(seq >> 8)   // sequence
-    p[7] = uint8(seq & 0xff) // sequence
-
-    // calculate icmp checksum
-    cklen := len(p)
-    s := uint32(0)
-    for i := 0; i < (cklen - 1); i += 2 {
-        s += uint32(p[i+1])<<8 | uint32(p[i])
-    }
-    if cklen&1 == 1 {
-        s += uint32(p[cklen-1])
-    }
-    s = (s >> 16) + (s & 0xffff)
-    s = s + (s >> 16)
-
-    // place checksum back in header; using ^= avoids the
-    // assumption the checksum bytes are zero
-    p[2] ^= uint8(^s & 0xff)
-    p[3] ^= uint8(^s >> 8)
-
-    return p
-}
-
-func parsePingReply(p []byte) (id, seq int) {
-    id = int(p[4])<<8 | int(p[5])
-    seq = int(p[6])<<8 | int(p[7])
-    return
-}
-
-func elapsedTime(start int) float32 {
-    t := float32(time.Now().Nanosecond() - start) / 1000000.0
-    return t
+// copy paste from http://code.google.com/p/go/source/browse/ipv4/helper_unix.go?repo=net#38
+var errInvalidConnType = errors.New("invalid conn type")
+func sysfd(c net.Conn) (int, error) {
+        cv := reflect.ValueOf(c)
+        switch ce := cv.Elem(); ce.Kind() {
+        case reflect.Struct:
+                netfd := ce.FieldByName("conn").FieldByName("fd")
+                switch fe := netfd.Elem(); fe.Kind() {
+                case reflect.Struct:
+                        fd := fe.FieldByName("sysfd")
+                        return int(fd.Int()), nil
+                }
+        }
+        return 0, errInvalidConnType
 }
 
 func main() {
-    flag.Parse()
-    dst := flag.Arg(0)
-    //dst := "121.254.177.105"
 
-    raddr, err := net.ResolveIPAddr("ip4", dst) // *IPAddr 
-    if err != nil {
-        log.Fatalf(`net.ResolveIPAddr("ip4", %v") = %v, %v`, dst, raddr, err)
-    }
-
-    ipconn, err := net.DialIP("ip4:icmp", nil, raddr) // *IPConn (Conn 인터페이스 구현)
-    if err != nil {
-        log.Fatalf(`net.DialIP("ip4:icmp", %v) = %v`, ipconn, err)
-    }
-
-    sendid := os.Getpid() & 0xffff
-    sendseq := 1
-    pingpktlen := 64
-
-    for {
-        sendpkt := makePingRequest(sendid, sendseq, pingpktlen, []byte("Go Ping"))
-
-        //fmt.Printf("%v, %v\n", sendpkt[0:8], string(sendpkt[8:]))
+        sendpkt := []byte("HEAD / HTTP/1.1")
 
         start := time.Now().Nanosecond()
 
-        n, err := ipconn.WriteToIP(sendpkt, raddr)
-        if err != nil || n != pingpktlen {
-            log.Fatalf(`net.WriteToIP(..., %v) = %v, %v`, raddr, n, err)
+        raddr, err := net.ResolveTCPAddr("tcp4", "www.google.com:80")
+        conn, err := net.DialTCP("tcp", nil, raddr)
+        fd, err := sysfd(conn)
+
+        ttl, err := syscall.GetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_TTL)
+
+        fmt.Println("Default ttl =", ttl)
+
+        _, _, _ = err, sendpkt, start
+
+        for c_ttl:=0; c_ttl <= ttl; c_ttl++ {
+            syscall.SetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_TTL, c_ttl)
+            fmt.Println("Now ttl =", c_ttl)
+            conn.Write(sendpkt)
+            time.Sleep(1000 * time.Millisecond)
         }
-
-        // ipconn.SetTimeout(5e8) // 0.5 second
-
-        resp := make([]byte, 1024)
-        for {
-            n, from, err := ipconn.ReadFrom(resp)
-            fmt.Printf("%d bytes from %s: icmp_req=%d time=%.2f ms\n", n, dst, sendseq, elapsedTime(start))
-
-            // log.Printf("%x", resp)
-
-            if err != nil {
-                log.Fatalf(`ReadFrom(...) = %v, %v, %v`, n, from, err)
-            }
-            if resp[0] != ICMP_ECHO_REPLY {
-                continue
-            }
-            rcvid, rcvseq := parsePingReply(resp)
-            if rcvid != sendid || rcvseq != sendseq {
-                log.Fatalf(`Ping reply saw id,seq=0x%x,0x%x (expected 0x%x, 0x%x)`, rcvid, rcvseq, sendid, sendseq)
-            }
-            break
-        }
-
-        sendseq++
-        time.Sleep(1e9) // 1 second. like -i (interval) option
-    }
+        
 }
